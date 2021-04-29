f2ee42f5f88ba1ab2c997a73111ba0bb291831df
matthewmjm <matthewmjm@yahoo.com>
2021-04-28 18:46:24 -0600

    branch for Dynamic Routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Layout from '../../components/Layout'
-import Date from '../../components/Date'
-import utilStyles from '../../styles/utils.module.css'
-import { getAllPostIds, getPostData } from '../../lib/getPosts'
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import Date from "../../components/Date";
+import utilStyles from "../../styles/utils.module.css";
+import { getAllPostIds, getPostData } from "../../lib/getPosts";
 
 export default function Post({ postData }) {
 	return (
@@ -11,20 +11,20 @@ export default function Post({ postData }) {
 				<title>{postData.title}</title>
 			</Head>
 			<Layout>
-				{/* <div className={utilStyles.postContainer}> */}
 				<div className={utilStyles.main}>
-					{/* <article> */}
 					<div className={utilStyles.headingXl}>{postData.title}</div>
 					<br />
 					<div className={utilStyles.calendar}>
-						<Date dateString={postData.date} />
+						<div>
+							<Date dateString={postData.date} />
+						</div>
 					</div>
 					<img
 						className={utilStyles.photo}
 						src={postData.image}
-						width='475'
-						height='475'
-						alt='{postData.title}'
+						width="475"
+						height="475"
+						alt="{postData.title}"
 					/>
 
 					<br />
@@ -38,24 +38,24 @@ export default function Post({ postData }) {
 				{/* </div> */}
 			</Layout>
 		</>
-	)
+	);
 }
 
 export async function getStaticPaths() {
-	const paths = getAllPostIds()
+	const paths = getAllPostIds();
 	return {
 		paths,
 		fallback: false,
-	}
+	};
 }
 
 export async function getStaticProps({ params }) {
-	const postData = await getPostData(params.id) // await
+	const postData = await getPostData(params.id); // await
 	return {
 		props: {
 			postData,
 		},
-	}
+	};
 }
 
 // export async function getStaticPaths() {
